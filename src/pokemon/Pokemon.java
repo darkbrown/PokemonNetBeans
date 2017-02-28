57d4a80687590cd24833e5087ed0d1409c9694e1
cris_ <cris_@DESKTOP-GF2A7MD>
2017-02-28 13:53:08 -0600

    Con clase de imagenes

---
+++ src/pokemon/Pokemon.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+
+/**
+ *
+ * @author cris_
+ */
+public class Pokemon {
+   public static Principal inicio = new Principal();
+   public static Formulario form = new Formulario();
+  
+    
+   public static void setVisibleInicio(){
+       inicio.setVisible(true);
+   }
+   public static void setVisibleFormulario(){
+       form.setVisible(true);
+   }
+    
+    public static void main(String[] args){
+    System.out.println("Directorio ejecucion = " + System.getProperty("user.dir"));     
+    form.setVisible(true);
+    inicio.setVisible(true);
+    
+        
+    }
+}
---
+++ src/recursos/Imagenes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursos;
+
+import javax.swing.ImageIcon;
+
+
+
+/**
+ *
+ * @author cris_
+ */
+public  class Imagenes {
+    
+    public static ImageIcon getImgFaltante(){
+ 		ImageIcon icon = new ImageIcon("src/recursos/no_image.jpg");
+                return icon;
+    }
+    
+}
